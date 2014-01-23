204c5e259c6f72349f39d75e4bd324dca185e110
Zidong Jiang <jiangzidong01@gmail.com>
2014-01-23 15:14:44 -0800

    add js into index

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,47 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("HAHA CSE170");
+		$("#testjs").text("Please wait...");
+		//$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+	$("#submitBtn").click(refreshPage);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#1f6dde");
+    /*var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");*/
+    var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) 
+    { 
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+    }
+    else 
+    { 
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(description).fadeOut();
+    }
+}
+
+function refreshPage(e){
+	var projectID = $('#project').val();
+   $(projectID).animate({
+      width: $('#width').val()
+   });
+   var changeText = $('#description').val();
+	/*var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); */
+    $(projectID + " .project-description").text(changeText);
 }
